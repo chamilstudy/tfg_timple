13df520f0fae5288ee2c43a107562038183e8739
Chamil <chamil.workacc@gmail.com>
2026-03-09 01:28:11 +0000

    style(settings-menu): chevron icon added and overflow hidden for long texts

--- components/ui/settings-menu/settings-menu.tsx
+++ components/ui/settings-menu/settings-menu.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { cn } from "@/lib/utils";
 
 import { InfoMessage } from "../input/info-message";
-import { title } from "process";
+import { ChevronRight } from "lucide-react";
 
 type Option = {
   icon?: ReactNode;
@@ -26,11 +26,11 @@ export default function SettingsMenu({
   return (
     <div
       className={cn(
-        "grid grid-flow-row rounded divide-y border overflow-hidden w-full shadow min-h-32 transition-all ease-in-out",
+        "grid grid-flow-row rounded divide-y border w-full shadow min-h-32 transition-all ease-in-out overflow-hidden",
         isLoading
-          ? " bg-muted border-background divide-background shadow-none text-transparent animate-pulse"
+          ? "bg-muted border-background divide-background shadow-none text-transparent animate-pulse"
           : "bg-card border-border divide-border",
-        fixedSize && "auto-rows-fr"
+        fixedSize && "auto-rows-fr",
       )}
     >
       {options.map((option, index) => (
@@ -38,12 +38,12 @@ export default function SettingsMenu({
           key={index}
           role="button"
           aria-label={option.title}
-          tabIndex={0}
+          tabIndex={isLoading ? -1 : 0}
           className={cn(
-            "flex items-center justify-between py-3 px-6 ",
+            "flex items-center justify-between py-3 px-6",
             isLoading
               ? "cursor-progress"
-              : "hover:bg-secondary hover:text-secondary-foreground cursor-pointer"
+              : "hover:bg-secondary hover:text-secondary-foreground cursor-pointer",
           )}
           onClick={option.action}
         >
@@ -51,11 +51,14 @@ export default function SettingsMenu({
             {option.icon}
             <div className="grid w-full">
               <p className="text-xs font-bold">{option.title}</p>
-              <p>{option.content}</p>
+              <p className="overflow-hidden text-ellipsis whitespace-nowrap">
+                {option.content}
+              </p>
               {option.info && (
                 <InfoMessage message={option.info} variant="default" />
               )}
             </div>
+            <ChevronRight className="text-border" />
           </div>
         </div>
       ))}
